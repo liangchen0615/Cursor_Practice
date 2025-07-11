bc874623595bff22cab788876a546eb6fad9e73a
liangchen0615 <liangchen0615@163.com>
2025-07-11 18:07:31 +0800

    Initial UI Skeleton for Wordle

---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,22 @@
+// pages/index/index.js
+Page({
+  data: {
+    guesses: [
+      ['', '', '', '', ''],
+      ['', '', '', '', ''],
+      ['', '', '', '', ''],
+      ['', '', '', '', ''],
+      ['', '', '', '', ''],
+      ['', '', '', '', '']
+    ],
+    keyboard: [
+      ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+      ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
+      ['Z', 'X', 'C', 'V', 'B', 'N', 'M']
+    ],
+    currentGuess: 0
+  },
+  onLoad() {
+    // Placeholder for future logic
+  }
+}) 
